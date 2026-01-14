492cb93ee500273ee812ec2ba3d5ba9b06d3a10e
Administrator <admin@example.local>
2026-01-14 14:30:58 +0530

    front

---
+++ backend/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend server is running 🚀");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
